f83c139ff0f7a7be0c59e2420257f19270412774
Alif Abdullah <aabdullah20@stuy.edu>
2021-10-17 18:16:02 -0400

    changing 14_form code after feedback from evaluation sheet

--- 14_form/app.py
+++ 14_form/app.py
@@ -3,22 +3,21 @@
 # K14 -- To be able to integrate forms and render responses on template files
 # 2021-10-14
 
+# Import statements. Also initializes the app object
 from flask import Flask, render_template, request 
 app = Flask(__name__)
 
+# Function to render, via template, the initial page, in this case the login page, with the text field for a username.
 @app.route("/")
 def login_form():
-    return render_template("login.html",tnpg="The Best Laptop: Alif Abdullah, Kevin Cao, Jonathan Wu")
+    return render_template("login.html")
 
+# Function to render, via template, the response page, which displays the username passed in the login page as well as the request method used. 
 @app.route("/auth")
 def response():
-    req_method = ""
-    if request.method == 'POST':
-        req_method = 'post'
-    elif request.method == 'GET':
-        req_method = 'get'
-    return render_template("response.html", tnpg="The Best Laptop: Alif Abdullah, Kevin Cao, Jonathan Wu",username=request.args['username'], greeting="We apologize for the inconvenience, your Exaltedness.",request_method=req_method)
-    
+    return render_template("response.html",username=request.args['username'],request_method=request.method)
+
+# Allows program to auto re-run in case of a change in code.
 if __name__ == "__main__":
     app.debug = True
     app.run()
