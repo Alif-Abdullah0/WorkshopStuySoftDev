b06fe55ee4ff46b5e3db50dcff149eda13aad5a1
Alif-Abdullah0 <alifabdullah862@gmail.com>
2022-02-09 08:57:01 -0500

    semi-colon fix

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,16 +1,16 @@
 function fact(n){
     if (n == 0){
-        return 1
+        return 1;
     } else {
-        return fact(n - 1) * n
+        return fact(n - 1) * n;
     }
 }
 
 function fib(n){
     if (n <= 1){
-        return n
+        return n;
     } else {
-        return fib(n - 1) + fib(n - 2)
+        return fib(n - 1) + fib(n - 2);
     }
 }
 
