a38c7e6ce28b33e448f2bbd0f5a246d71d0b09df
Alif-Abdullah0 <alifabdullah862@gmail.com>
2022-02-15 08:51:50 -0500

    late submission

--- 30_jsdraw/clickdraw.js
+++ 30_jsdraw/clickdraw.js
@@ -1,38 +1,77 @@
-let c = document.getElementById('slate');
-let ctx = c.getContext("2d");
-let mode = "rect";
-let toggleMode = (e) => {
-  console.log("toggling...");
-  if (mode == "rect") {
-    mode = "circ";
-  } else {
-    mode = "rect";
-  }
+//retrieve node in DOM via ID
+var c = document.getElementById('slate');
+
+var ctx = c.getContext("2d");
+
+var mode = 'rect';
+var fill = false;
+
+var toggleMode = function(e) {
+    console.log("toggling...");
+    if (mode == 'rect') {
+        mode = 'circle';
+    }
+    else {
+        mode = 'rect';
+    }
 }
 
-let drawRect = function(e) {
-  let mouseX = offsetX;
-  let mouseY = offsetY;
-  console.log("mouseclick registered at ", mouseX, mouseY);
+var clearMode = function() {
+    console.log("erase mode...");
+    mode = 'erase'
 }
 
-let drawCircle = (e) => {
-  let mouseX = offsetX;
-  let mouseY = offsetY;
-  console.log("mouseclick registered at ", mouseX, mouseY);
+var drawRect = function(e) {
+    ctx.strokeStyle = 'red'
+    ctx.fillStyle = 'red'
+
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+
+    if (fill) {ctx.fillRect(0, 0, mouseX, mouseY);}
+    else {ctx.strokeRect(0, 0, mouseX, mouseY);}
+
+    console.log("mouseclick registered at ", mouseX, mouseY);
 }
 
-let draw = (e) => {
-  if (mode == "rect") {drawRect(ctx);}
-  else {drawCircle(ctx);}
+var drawCircle = function(e) {
+    ctx.strokeStyle = 'blue'
+    ctx.fillStyle = 'blue'
+
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, Math.min(mouseX, mouseY, 600-mouseX, 600-mouseY), 0, 2* Math.PI);
+
+    if (fill) {ctx.fill();}
+    else {ctx.stroke()}
+
+    console.log("mouseclick registered at AJHAHAAHAHAH", mouseX, mouseY);
 }
 
-let wipeCanvas = () => {
-  ctx.clearRect(0, 0, c.width, c.height);
+var erase = function(e) {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+
+    ctx.clearRect(mouseX-150, mouseY-100, 300, 200);
 }
 
-c.addEventListener("click", draw);
-let bToggler = document.getElementById("buttonToggle");
-bToggler.addEventListener("click", toggleMode);
-let clearB = document.getElementById("button);
-clearB.addEventListener();
+var draw = (e) => {
+    if (mode == 'rect') {drawRect(event);}
+    else if (mode == 'circle') {drawCircle(event);}
+    else {erase(event);}
+}
+
+c.addEventListener('click', draw);
+var bToggler = document.getElementById('buttonToggle');
+bToggler.addEventListener('click', toggleMode);
+
+var clearB = document.getElementById('buttonClear');
+clearB.addEventListener('click', clearMode);
+
+var bLine = document.getElementById('buttonLine');
+bLine.addEventListener('click', function(){fill=false});
+
+var bFill = document.getElementById('buttonFill');
+bFill.addEventListener('click', function(){fill=true});
