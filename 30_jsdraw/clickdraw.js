778a5a634faf4818f82e4b17d8a6dcd89987c502
Alif-Abdullah0 <alifabdullah862@gmail.com>
2022-02-15 05:42:07 -0500

    changes

--- 30_jsdraw/clickdraw.js
+++ 30_jsdraw/clickdraw.js
@@ -1,1 +1,38 @@
 let c = document.getElementById('slate');
+let ctx = c.getContext("2d");
+let mode = "rect";
+let toggleMode = (e) => {
+  console.log("toggling...");
+  if (mode == "rect") {
+    mode = "circ";
+  } else {
+    mode = "rect";
+  }
+}
+
+let drawRect = function(e) {
+  let mouseX = offsetX;
+  let mouseY = offsetY;
+  console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+let drawCircle = (e) => {
+  let mouseX = offsetX;
+  let mouseY = offsetY;
+  console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+let draw = (e) => {
+  if (mode == "rect") {drawRect(ctx);}
+  else {drawCircle(ctx);}
+}
+
+let wipeCanvas = () => {
+  ctx.clearRect(0, 0, c.width, c.height);
+}
+
+c.addEventListener("click", draw);
+let bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click", toggleMode);
+let clearB = document.getElementById("button);
+clearB.addEventListener();
